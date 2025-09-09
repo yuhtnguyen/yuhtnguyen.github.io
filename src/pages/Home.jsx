18e631a0bb30c57fb42874fe348833a10bb2d133
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-09-09 19:48:10 +0700

    delete: content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { Typography, Button, Row, Col, Carousel } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
 
